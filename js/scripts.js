4f37c96f39f60cc2aa2a6f314c61440c3a4c4724
Christina Dobi <iamchristinabot@gmail.com>
2017-03-19 10:08:30 -0400

    Add animations to homepage

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 		offset: '75%'
 	});
 	$('.wp3').waypoint(function() {
-		$('.wp3').addClass('animated bounceInDown');
+		$('.wp3').addClass('animated fadeInDown');
 	}, {
 		offset: '75%'
 	});
@@ -31,38 +31,9 @@ $(document).ready(function() {
 		prevNextButtons: false
 	});
 
-	$('#showcaseSlider').flickity({
-		cellAlign: 'left',
-		contain: true,
-		prevNextButtons: false,
-		imagesLoaded: true
-	});
-
-	/***************** Fancybox ******************/
-
-	$(".youtube-media").on("click", function(e) {
-		var jWindow = $(window).width();
-		if (jWindow <= 768) {
-			return;
-		}
-		$.fancybox({
-			href: this.href,
-			padding: 4,
-			type: "iframe",
-			'href': this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
-		});
-		return false;
-	});
-
 });
 
-$(document).ready(function() {
-	$("a.single_image").fancybox({
-		padding: 4,
-	});
-});
-
-/***************** Nav Transformicon ******************/
+/***************** Mobile Nav ******************/
 
 /* When user clicks the Icon */
 $(".nav-toggle").click(function() {
